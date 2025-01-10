85d85c159ee6aed2bab0f41991d1da3688e71c8d
scraly <scraly@gmail.com>
2025-01-10 15:07:14 +0100

    fix: Q10 script every time - golang>21 to 20

--- q10.sh
+++ q10.sh
@@ -15,14 +15,14 @@ clear
 cd gophers-api
 
 p "Build d'une image"
-pe 'docker build . -t gophers-api:friday'
+pe 'docker build . -t gophers-api:friday -f Dockerfile-golang21'
 
 pe 'docker image ls gophers-api'
 
-p "Changement de golang 1.20 vers 1.21"
-pe 'vi Dockerfile'
+p "Changement de golang 1.21 vers 1.20"
+pe 'vi Dockerfile-golang21'
 
-pe 'docker build . -t gophers-api:friday'
+pe 'docker build . -t gophers-api:friday -f Dockerfile-golang21'
 
 pe 'docker image ls gophers-api'
 
@@ -33,4 +33,12 @@ pe "docker rmi \$(docker images -f dangling=true -q)"
 
 p "/!\ Docker only - Les dangling images n'existent pas avec containerd en image store"
 
+#Revert Dockerfile-golang21 golang version
+if [[ "$OSTYPE" != "darwin"* ]]; then
+    sed -i 's/1.20/1.21/g' Dockerfile-golang21
+else
+    sed -i '' 's/1.20/1.21/g' Dockerfile-golang21
+fi
+
 cd ..
+p "Fini !"
