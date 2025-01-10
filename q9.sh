bdcf137817315d5c0b9c880e9b239127e8fc97a6
sherine-k <skhoury@redhat.com>
2025-01-10 14:12:22 +0100

    Q9: Remove sbom attach

--- q9.sh
+++ q9.sh
@@ -30,22 +30,22 @@ pe 'trivy image --format spdx-json --output /tmp/result.json 79352h8v.c1.de1.con
 p 'Visualiser le spdx, avec bom'
 pe 'bom document outline /tmp/result.json'
 
-p '# Tips: on peut utiliser bom, docker et podman pour générer un SBOM'
-p '# docker scout sbom ubuntu --output sbom.txt'
-p '# podman build . -t myimage:mytag --sbom-scanner-command trivy --sbom-output sbom.txt'
-p '# bom generate --image quay.io/skhoury/ubi9/ubi-micro:latest --output=sbom.json --format json'
+p 'Tips: on peut utiliser bom, docker et podman pour générer un SBOM'
+p 'docker scout sbom ubuntu --output sbom.txt'
+p 'podman build . -t myimage:mytag --sbom-scanner-command trivy --sbom-output sbom.txt'
+p 'bom generate --image quay.io/skhoury/ubi9/ubi-micro:latest --output=sbom.json --format json'
 pe 'bom document outline scout_sbom.json'
 pe 'bom document outline bom_sbom.json'
 
-p '# Attachons le sbom à l''\image'
-pe 'oras attach 79352h8v.c1.de1.container-registry.ovh.net/public/ubi9/ubi-micro:latest --artifact-type application/spdx+json trivy_sbom.json:text/spdx+json'
+# p '# Attachons le sbom à l''\image'
+# pe 'oras attach 79352h8v.c1.de1.container-registry.ovh.net/public/ubi9/ubi-micro:latest --artifact-type application/spdx+json trivy_sbom.json:text/spdx+json'
 
-p '# Regardons la registry... https://79352h8v.c1.de1.container-registry.ovh.net/harbor/projects/2/repositories/ubi9%2Fubi-micro/artifacts-tab'
+# p '# Regardons la registry... https://79352h8v.c1.de1.container-registry.ovh.net/harbor/projects/2/repositories/ubi9%2Fubi-micro/artifacts-tab'
 
-pe 'oras discover 79352h8v.c1.de1.container-registry.ovh.net/public/ubi9/ubi-micro:latest'
+# pe 'oras discover 79352h8v.c1.de1.container-registry.ovh.net/public/ubi9/ubi-micro:latest'
 
-p 'oras discover sherinefedora:5000/ubi9/ubi-micro:latest'
-cat oras_discover_distrib.out
+# p 'oras discover sherinefedora:5000/ubi9/ubi-micro:latest'
+# cat oras_discover_distrib.out
 
 cd ..
 p "Fini !"
